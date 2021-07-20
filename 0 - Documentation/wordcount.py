70ddb72f295e967c56b895739ea2a66a16e041d8
cmc-2020 <christopher.chandler@outlook.de>
2021-07-20 19:49:23 +0200

    update
    update

--- 0 - Documentation/wordcount.py
+++ 0 - Documentation/wordcount.py
@@ -5,13 +5,11 @@ doc="Chandler_Linguistik_B_A_Theisis_SoSe2021.docx"
 my_text = docx2txt.process(doc)
 
 #Characters to not be counted
-non_count=6229
 limit=75000
 
 doc_count=len([char for char in my_text])
 
-charcount=round(doc_count-non_count-(doc_count*0.0015))
+charcount=round(doc_count-(doc_count*0.171))
 res=round(charcount/limit*100,2)
 print(f"{charcount} written out of {limit}.")
 print(f"{res} has been completed")
-
