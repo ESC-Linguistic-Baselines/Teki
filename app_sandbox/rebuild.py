2bff32b20525f6ae089dc309198360295b6b5dd7
cmc-2020 <christopher.chandler@outlook.de>
2021-08-05 13:54:53 +0200

    update
    update

--- app_sandbox/rebuild.py
+++ app_sandbox/rebuild.py
@@ -11,8 +11,8 @@ def _rebuild_requierement_resources():
         There is no object, but a file is created that is placed in the main directory.py
     """
 
-    with open("requirement_resources.txt", mode="w+", encoding="utf-8") as resources:
-        for path, subdirs, files in os.walk("app_resources"):
+    with open("../requirement_resources.txt", mode="w+", encoding="utf-8") as resources:
+        for path, subdirs, files in os.walk("../app_resources"):
             for name in files:
                 resources.write(os.path.join(path, name)+"\n")
     print("The app resource directory.py file has been updated.")
