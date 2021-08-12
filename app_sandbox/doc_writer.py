8a76c237ec2e340caae04b1c1efeef904c8e231f
cmc-2020 <christopher.chandler@outlook.de>
2021-08-12 20:05:10 +0200

    update
    update

--- app_sandbox/doc_writer.py
+++ app_sandbox/doc_writer.py
@@ -3,16 +3,16 @@ import json
 
 def oral ():
 
-    argot=open("../app_program_resources/default_files/mueller_registers/FA.txt", mode="r", encoding="utf-8")
-    argot_suffix=open("../app_program_resources/default_files/mueller_registers/FA_suf.txt", mode="r", encoding="utf-8")
-    emoticons=open("../app_program_resources/default_files/mueller_registers/emoticons.csv", mode="r", encoding="utf-8")
+    argot=open("../app_program_resources/default_files/french_registers/FA.txt", mode="r", encoding="utf-8")
+    argot_suffix=open("../app_program_resources/default_files/french_registers/FA_suf.txt", mode="r", encoding="utf-8")
+    emoticons=open("../app_program_resources/default_files/french_registers/emoticons.csv", mode="r", encoding="utf-8")
     emoticon_symbols=[i.split(",")[0] for i in emoticons]
 
-    francais_vulgaire=open("../app_program_resources/default_files/mueller_registers/FV.txt", mode="r", encoding="utf-8")
-    fpa=open("../app_program_resources/default_files/mueller_registers/FPA.txt", mode="r", encoding="utf-8")
-    francais_familier=open("../app_program_resources/default_files/mueller_registers/FF.txt", mode="r", encoding="utf-8")
-    francais_familier_inst=open("../app_program_resources/default_files/mueller_registers/FF_inst.txt", mode="r", encoding="utf-8")
-    presentatifs=open("../app_program_resources/default_files/mueller_registers/FA_pres.txt", mode="r", encoding="utf-8").read().split()
+    francais_vulgaire=open("../app_program_resources/default_files/french_registers/FV.txt", mode="r", encoding="utf-8")
+    fpa=open("../app_program_resources/default_files/french_registers/FPA.txt", mode="r", encoding="utf-8")
+    francais_familier=open("../app_program_resources/default_files/french_registers/FF.txt", mode="r", encoding="utf-8")
+    francais_familier_inst=open("../app_program_resources/default_files/french_registers/FF_inst.txt", mode="r", encoding="utf-8")
+    presentatifs=open("../app_program_resources/default_files/french_registers/FA_pres.txt", mode="r", encoding="utf-8").read().split()
 
 
     with open ("../user_databases/oral_french.json", mode="w", encoding="utf-8") as oral_file:
@@ -34,12 +34,12 @@ def oral ():
 def lit ():
     import json
 
-    francais_cultive = open("../app_program_resources/default_files/mueller_registers/FCL.txt", mode="r", encoding="utf-8").read().split()
-    francais_cultive_abs = open("../app_program_resources/default_files/mueller_registers/FCL_abs.txt", mode="r", encoding="utf-8").read().split()
-    francais_ecrit = open("../app_program_resources/default_files/mueller_registers/FRE.txt", mode="r", encoding="utf-8").read().split()
-    francais_technique = open("../app_program_resources/default_files/mueller_registers/FL.txt", mode="r", encoding="utf-8").read().split()
-    francais_technique_suf = open("../app_program_resources/default_files/mueller_registers/FL_suf.txt", mode="r", encoding="utf-8").read().split()
-    francais_technique_pre = open("../app_program_resources/default_files/mueller_registers/Fl_pre.txt", mode="r", encoding="utf-8").read().split()
+    francais_cultive = open("../app_program_resources/default_files/french_registers/FCL.txt", mode="r", encoding="utf-8").read().split()
+    francais_cultive_abs = open("../app_program_resources/default_files/french_registers/FCL_abs.txt", mode="r", encoding="utf-8").read().split()
+    francais_ecrit = open("../app_program_resources/default_files/french_registers/FRE.txt", mode="r", encoding="utf-8").read().split()
+    francais_technique = open("../app_program_resources/default_files/french_registers/FL.txt", mode="r", encoding="utf-8").read().split()
+    francais_technique_suf = open("../app_program_resources/default_files/french_registers/FL_suf.txt", mode="r", encoding="utf-8").read().split()
+    francais_technique_pre = open("../app_program_resources/default_files/french_registers/Fl_pre.txt", mode="r", encoding="utf-8").read().split()
 
     # writing to json file
     with open("../user_databases/lit_french.json", mode="w", encoding="utf-8") as oral_file:
